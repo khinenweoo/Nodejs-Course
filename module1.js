3c49009ba404ee3373dfcbfb9f3b91ab9da92959
Khine Nwe Oo <devkhinenweoo@gmail.com>
2024-08-11 21:08:20 +0630

    episode 4 - node module

---
+++ module1.js
@@ -0,0 +1,9 @@
+const myname = 'khinenweoo';
+const myage = 32;
+const isAnother = true;
+
+module.exports = {
+    myname,
+    myage,
+    isAnother
+};
---
+++ module2.js
@@ -0,0 +1,3 @@
+const {myname,myage, isAnother} = require('./module1');
+
+console.log(myage, myname, `is Female? ${isAnother}`);
